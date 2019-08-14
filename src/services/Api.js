6dd830cfb45f182e98d83b2bd49671f2d086d8a0
Tommling <madefortv@yahoo.com>
2019-08-14 01:15:08 -0400

    add protocl to cors origin header

--- src/services/Api.js
+++ src/services/Api.js
@@ -8,7 +8,7 @@ export default() => {
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': process.env.VUE_APP_CORS 
+            'Access-Control-Allow-Origin': process.env.VUE_APP_ROOT_API
         }
     })
 }
--- src/services/FactomService.js
+++ src/services/FactomService.js
@@ -8,7 +8,7 @@ export default() => {
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': process.env.VUE_APP_CORS_FACTOM 
+            'Access-Control-Allow-Origin': process.env.VUE_APP_FACTOM_API
         }
     })
 }
